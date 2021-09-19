5edab5b50d70364518fc3d0f3f25b7c7855e9c5b
sxy <sxy@qq.com>
2021-09-19 10:40:54 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -13,5 +13,6 @@ public class gitTest {
         System.out.println("hello,git");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
+        System.out.println("hello,git4");
     }
 }
