d962711c35fa1b7c8d122b247bd2bc2994187e6c
sxy <sxy@qq.com>
2021-09-19 10:27:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName gitTest
+ * @Description TODO
+ * @Author SXY
+ * @Date 2021/9/19 10:23
+ * @Version 1.0
+ */
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
